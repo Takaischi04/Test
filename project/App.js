50b4755bb08c3cf9e3af7fc7dcbf297bd7fabe49
Diego <you@example.com>
2024-04-19 14:07:14 +0200

    Icons/pngs

--- project/App.js
+++ project/App.js
@@ -4,11 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TEST</Text>
       <StatusBar style="auto" />
-      <div>
-        <p>Testgit </p>
-      </div>
     </View>
   );
 }
